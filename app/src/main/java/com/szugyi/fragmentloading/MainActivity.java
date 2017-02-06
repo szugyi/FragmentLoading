f4deeaf84aaa5dc876a367e92775bbd3915f6470
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-02-06 22:21:14 +0100

    Init

---
+++ app/src/main/java/com/szugyi/fragmentloading/MainActivity.java
@@ -0,0 +1,13 @@
+package com.szugyi.fragmentloading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
